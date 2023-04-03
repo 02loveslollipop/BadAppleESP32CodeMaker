e855f8eb20876c17f2f809022f5f01b6bd1a42df
02loveslollipop <mmonsalve2005@gmail.com>
2023-04-02 21:11:27 -0500

    Functionaly implementation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import cv2
 import numpy as np
 from PIL import Image
+import os
 
 def count_frames(path: str, manual:str=False):
     if manual:
@@ -56,10 +57,10 @@ def convertion(height: int,width: int,video):
         imgArray = np.asarray(image)
         for i in range(height-1):
             for j in range(width-1):
-                if imgArray[i][j][0] >= 128 and imgArray[i][j][1] >= 128 and imgArray[i][j][2] >= 128:
-                    vector[i][j] = False
-                else:
+                if imgArray[i][j][0] >= 90 and imgArray[i][j][1] >= 90 and imgArray[i][j][2] >= 90:
                     vector[i][j] = True
+                else:
+                    vector[i][j] = False
         print(f"Cached frame: {count+1}")
         l_vectors.append(vector) 
         count += 1
@@ -76,10 +77,37 @@ def findSubFrame(list_buffer,subframe):
             
     raise Exception("subFrame not found")
 
+def clear():
+    if os.name == 'posix':
+        os.system('clear')
+    else:
+        os.system('cls')
+
+def SaveResultAsPhotoList(patternList,frameList,width,height,patternSize):
+    frameCount = 1
+    frameBuffer = np.zeros((width,height,3),dtype=float)
+    for frame in frameList:
+        for i in range(int(width/l_pattern)-1):
+            for j in range(int(height/l_pattern)-1):
+                bufferSubframe = patternList[frame[i][j]]  
+                for subframe_i in range(patternSize):
+                    for subframe_j in range(patternSize):
+                        if bufferSubframe[subframe_i][subframe_j]:
+                            frameBuffer[(i*patternSize)+(subframe_i)][(j*patternSize)+(subframe_j)][0] = 0
+                            frameBuffer[(i*patternSize)+(subframe_i)][(j*patternSize)+(subframe_j)][1] = 0
+                            frameBuffer[(i*patternSize)+(subframe_i)][(j*patternSize)+(subframe_j)][2] = 0
+                        else:
+                            frameBuffer[(i*patternSize)+(subframe_i)][(j*patternSize)+(subframe_j)][0] = 255
+                            frameBuffer[(i*patternSize)+(subframe_i)][(j*patternSize)+(subframe_j)][1] = 255
+                            frameBuffer[(i*patternSize)+(subframe_i)][(j*patternSize)+(subframe_j)][2] = 255
+        cv2.imwrite(filename=f"{frameCount:05d}.png",img=frameBuffer)
+        frameCount += 1
+
 args = sys.argv
 #print(args)
-#args = ['c:/Users/Katana GF66 11UC/Documents/BadAppleESP32 Project/Code maker/main.py', '1.mp4', '-r', '16x8', '-lc']
+args = ['c:/Users/Katana GF66 11UC/Documents/BadAppleESP32 Project/Code maker/main.py', '1.mp4', '-r', '32x16', '-lc']
 if len(args) <= 1:
+    clear()
     print("You must give at least the video path")
     exit()
 else:
@@ -147,11 +175,16 @@ else:
             path = args[i]
             vid = cv2.VideoCapture(path)
             ready = True
+        else: 
+            clear()
+            print(f"{args[i]} has not been recognize as a valid argument")
 if not batch:
-    print(f"----SUMMARY----\nFile path: {path}\nDither: {dither}\nResample algoritm: {resample}\nHeight: {height}\nWidth: {width}\nThe convertion will start, Press 1 to make a test of the resample algoritm\npress anything else to start the convertion.")
+    clear()
+    print(f"----SUMMARY----\nFile path: {path}\nDither: {dither}\nResample algoritm: {resample}\nHeight: {height}\nWidth: {width}\nPress 1 to make a test of the algoritm method\nPress any other button to start the convertion")
     sel = input()
     if sel == "1":
-        print("Frame for testing: ")
+        clear()
+        print("Frame to test: ")
         frame = int(input())
         sus = True
         con = 0
@@ -160,9 +193,11 @@ if not batch:
             con += 1
         imgTest = cv2.resize(src=img,dsize=(height,width),interpolation=resample)
         cv2.imwrite(filename="test.png",img=imgTest)
+        clear()
         print("File save as test.png, press any button to start convertion\n\nDuring convertion press ESC to cancel the process")
         input()
     else:     
+        clear()
         print("Press any button to start convertion\n\nDuring convertion press ESC to cancel the process")
 
 if resize:
@@ -172,41 +207,53 @@ else:
 
 l_subframes = []
 l_frames_compress = []
-
-i = 0
-j = 0
-k = 0
-h_sprite = 0
-w_sprite = 0
-
-# de cada fotograma se coge sub fotogramas cuadrados de orden l_pattern x l_pattern, 
-# entonce hay que hacer que el programa coja desde 0 hasta 8 y lo meta en un vector, 
-# ahora mismo mi idea es que vaya sean 5 for (si, extremadamente eficiente),entonces 
-# el primero es el de los frames, luego el otro es el de los subframes horizontales y 
-# el otro los subframes verticales, el siguiente lo que hace es llenar los subframes 
-# entonces deben ser desde 0 hasta l_pattern, y yo creeria que deberia ser desde 1, 
-# porque hay que ver como solucionar lo de 0, porque eso me puede tirar problemas aunque 
-# creo que podria ser ((sprite_actual)*l_pattern)+(p_actual)) YO CREO Y ESPERO QUE SIRVA
+sub_frame_finded = 0
+sub_frame_not_finded = 0
+total_frames = 0
+clear()
+print("Creating and assigning subframe table")
 for frame in lFrames:
     subframe_map = np.zeros(shape=(int(width/l_pattern),int(height/l_pattern)),dtype=int)
     for i in range(len(subframe_map)):
         for j in range(len(subframe_map[0])):
             subframe = np.zeros(shape=(l_pattern,l_pattern),dtype=bool)
-            for subframe_i in range(l_pattern-1):
-                for subframe_j in range(l_pattern-1):
+            for subframe_i in range(l_pattern):
+                for subframe_j in range(l_pattern):
                     subframe[subframe_i][subframe_j] = frame[(i*l_pattern)+(subframe_i)][(j*l_pattern)+(subframe_j)]
             try:
-                val = findSubFrame(list=l_subframes,subframe=subframe)
-            except:
+                val = findSubFrame(list_buffer=l_subframes,subframe=subframe)
+                sub_frame_finded += 1
+            except Exception as e:
                 l_subframes.append(subframe)
                 val = int(len(l_subframes)-1)
-                print("new subframe created")
+                sub_frame_not_finded += 1
             finally:
                 subframe_map[i][j] = val
-                print("subframe calculated")
-                
+                total_frames += 1
     l_frames_compress.append(subframe_map)
-    print(l_subframes)                  
+clear()
+print(f"Total subframes: {total_frames}\nSubframes not compress: {sub_frame_not_finded}\nsubframes compress: {sub_frame_finded}")
+for frames in l_frames_compress:
+    print(frames)
+
+print("Press 1 to save result as Photo list\nPress anything else to exit the program")
+if input() == "1":
+    SaveResultAsPhotoList(l_subframes,l_frames_compress,width,height,l_pattern)
+    
+
+    
+                        
+
+                
+                    
+                    
+                        
+                        
+                        
+
+
+
+                   
                         
                     
                 
